3b14d59dcff4bbc4a3a9fe3264464ac9138c386a
abhishekl1289 <801305abhishekl@gmail.com>
2024-05-17 00:54:18 +0530

    connected user to a room

--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useRef,} from "react";
 import { useStateWithCallback } from "./useStateWithCallback";
 import {socketInit} from '../socket';
 import { ACTIONS } from "../actions";
-// const users=[{id: 1,name:'Rakesh K',},{id: 2,name: 'John Doe',}]
+import freeice from 'freeice';
 export const useWebRTC=(roomId,user)=>{
     const [clients,setClients]=useStateWithCallback([]);
     const audioElements=useRef({});
@@ -14,7 +14,7 @@ export const useWebRTC=(roomId,user)=>{
         socket.current=socketInit();
     },[]);
     
-    const addNewClients=useCallback((newClient,cb)=>{
+    const addNewClient=useCallback((newClient,cb)=>{
         const lookingFor=clients.find((client)=>client.id===newClient.id);
         if(lookingFor===undefined){
             setClients((existingClients)=>[...existingClients,newClient],cb);
@@ -26,8 +26,9 @@ export const useWebRTC=(roomId,user)=>{
             localMediaStream.current=await navigator.mediaDevices.getUserMedia({audio:true,});
         };
         startCapture().then(()=>{
-            addNewClients(user,()=>{
+            addNewClient(user,()=>{
                 const localElement=audioElements.current[user.id];
+                console.log(localElement.srcObject);
                 if(localElement){
                     localElement.volume=0;
                     localElement.srcObject=localMediaStream.current;
@@ -35,6 +36,116 @@ export const useWebRTC=(roomId,user)=>{
                 socket.current.emit(ACTIONS.JOIN,{roomId,user});
             });
         });
+        return ()=>{
+            localMediaStream.current.getTracks().forEach((track)=>track.stop());
+            socket.current.emit(ACTIONS.LEAVE,{roomId});
+        };
+    },[]);
+
+    useEffect(()=>{
+        const handleNewPeer=async({peerId,createOffer,user:remoteUser})=>{
+            if(peerId in connections.current){
+                return console.warn(`You are already connected with ${peerId} (${user.name})`);
+            }
+            connections.current[peerId]=new RTCPeerConnection({
+                iceServers:freeice()
+            });
+            connections.current[peerId].onicecandidate=(event)=>{
+                socket.current.emit(ACTIONS.RELAY_ICE,{
+                    peerId,
+                    icecandidate:event.candidate
+                })
+            }
+            connections.current[peerId].ontrack=({
+                streams:[remoteStream]
+            })=>{
+                addNewClient(remoteUser,()=>{
+                    if(audioElements.current[remoteUser.id]){
+                        // console.log(remoteStream);
+                        audioElements.current[remoteUser.id].srcObject=remoteStream;
+                    } else {
+                        let settled=false;
+                        const interval=setInterval(()=>{
+                            if(audioElements.current[remoteUser.id]){
+                                audioElements.current[remoteUser.id].srcObject=remoteStream;
+                                settled=true;
+                            }
+                            if(settled){
+                                clearInterval(interval);
+                            }
+                        },1000);
+                    }
+                });
+            };
+            localMediaStream.current.getTracks().forEach((track)=>{
+                connections.current[peerId].addTrack(
+                    track,
+                    localMediaStream.current
+                );
+            });
+            if(createOffer){
+                const offer=await connections.current[peerId].createOffer();
+                await connections.current[peerId].setLocalDescription(offer);
+                socket.current.emit(ACTIONS.RELAY_SDP,{
+                    peerId,
+                    sessionDescription:offer
+                })
+            }
+        };
+        socket.current.on(ACTIONS.ADD_PEER,handleNewPeer);
+        return()=>{
+            socket.current.off(ACTIONS.ADD_PEER);
+        };
+    },[]);
+
+    useEffect(()=>{
+        socket.current.on(ACTIONS.ICE_CANDIDATE,({peerId,icecandidate})=>{
+            if(icecandidate){
+                connections.current[peerId].addIceCandidate(icecandidate);
+            }
+        });
+        return ()=>{
+            socket.current.off(ACTIONS.ICE_CANDIDATE);
+        };
+    },[]);
+
+    useEffect(()=>{
+        const handleRemoteSdp=async({
+            peerId,
+            sessionDescription:remoteSessionDescription,
+        })=>{
+            connections.current[peerId].setRemoteDescription(
+                new RTCSessionDescription(remoteSessionDescription)
+            )
+            if(remoteSessionDescription.type==='offer'){
+                const connection=connections.current[peerId];
+                const answer=await connection.createAnswer();
+                connection.setLocalDescription(answer);
+                socket.current.emit(ACTIONS.RELAY_SDP,{
+                    peerId,
+                    sessionDescription:answer,
+                });
+            }
+        };
+        socket.current.on(ACTIONS.SESSION_DESCRIPTION,handleRemoteSdp);
+        return()=>{
+            socket.current.off(ACTIONS.SESSION_DESCRIPTION);
+        };
+    },[]);
+
+    useEffect(()=>{
+        const handleRemovePeer=async({peerId,userId})=>{
+            if(connections.current[peerId]){
+                connections.current[peerId].close();
+            }
+            delete connections.current[peerId];
+            delete audioElements.current[peerId];
+            setClients(list=>list.filter(client=>client.id!==userId));
+        };
+        socket.current.on(ACTIONS.REMOVE_PEER,handleRemovePeer);
+        return ()=>{
+            socket.current.off(ACTIONS.REMOVE_PEER);
+        };
     },[]);
     const provideRef=(instance,userId)=>{
         audioElements.current[userId]=instance;
